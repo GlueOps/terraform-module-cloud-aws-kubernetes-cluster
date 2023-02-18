abd88780d9a4c4f39333d4dee581587114581209
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-02-17 19:33:47 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -141,7 +141,7 @@ resource "aws_iam_role" "eks_addon_ebs_csi_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ebs_csi" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.eks_addon_ebs_csi_role.name
 }
 
