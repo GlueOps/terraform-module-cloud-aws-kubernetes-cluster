3e5727866e16cbae90ea0c7ea887507f17afe165
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-02-17 18:16:25 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -150,4 +150,5 @@ resource "aws_eks_addon" "ebs_csi" {
   addon_name        = "aws-ebs-csi-driver"
   addon_version     = "v1.15.0-eksbuild.1"
   resolve_conflicts = "OVERWRITE"
+  service_account_role_arn = aws_iam_role.eks_addon_ebs_csi_role.arn
 }
