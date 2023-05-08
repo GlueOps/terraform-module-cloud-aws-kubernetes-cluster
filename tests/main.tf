358a737d2cfd12382272d0d605cc803d58a8651d
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-05-08 02:47:19 +0000

    feat: adjust size of nodes

--- tests/main.tf
+++ tests/main.tf
@@ -9,9 +9,9 @@ module "captain" {
   node_pools = [
     # {
     #   "ami_image_id" : "amazon-eks-node-1.26-v20230411",
-    #   "instance_type" : "t3a.large",
+    #   "instance_type" : "t3a.small",
     #   "name" : "clusterwide-node-pool-1",
-    #   "node_count" : 2,
+    #   "node_count" : 1,
     #   "spot" : false,
     #   "disk_size_gb" : 20,
     #   "max_pods" : 1000
