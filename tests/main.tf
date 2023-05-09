c545688f81c56e885e00aed0e950026aeffd1115
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-05-08 17:42:52 -0700

    Update main.tf

--- tests/main.tf
+++ tests/main.tf
@@ -9,9 +9,9 @@ module "captain" {
   node_pools = [
     # {
     #   "ami_image_id" : "amazon-eks-node-1.26-v20230411",
-    #   "instance_type" : "t3a.medium",
+    #   "instance_type" : "t3a.small",
     #   "name" : "clusterwide-node-pool-1",
-    #   "node_count" : 1,
+    #   "node_count" : 2,
     #   "spot" : false,
     #   "disk_size_gb" : 20,
     #   "max_pods" : 1000
