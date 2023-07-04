1c5638634bbb59da58a3806c9e15f53854bfe5f7
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-07-04 03:06:38 +0000

    feat: update tests

--- tests/main.tf
+++ tests/main.tf
@@ -1,14 +1,15 @@
 module "captain" {
-  iam_role_to_assume = "arn:aws:iam::761182885829:role/glueops-captain"
-  source             = "../"
-  eks_version        = "1.26"
-  csi_driver_version = "v1.18.0-eksbuild.1"
-  vpc_cidr_block     = "10.65.0.0/26"
-  region             = "us-west-2"
-  availability_zones = ["us-west-2a", "us-west-2b"]
+  iam_role_to_assume           = "arn:aws:iam::761182885829:role/glueops-captain"
+  source                       = "../"
+  eks_version                  = "1.27"
+  csi_driver_version           = "v1.20.0-eksbuild.1"
+  vpc_cidr_block               = "10.65.0.0/26"
+  kubernetes_service_ipv4_cidr = "172.18.0.0/16"
+  region                       = "us-west-2"
+  availability_zones           = ["us-west-2a", "us-west-2b"]
   node_pools = [
     # {
-    #   "ami_image_id" : "amazon-eks-node-1.26-v20230411",
+    #   "ami_image_id" : "amazon-eks-node-1.27-v20230411",
     #   "instance_type" : "t3a.small",
     #   "name" : "clusterwide-node-pool-1",
     #   "node_count" : 2,
