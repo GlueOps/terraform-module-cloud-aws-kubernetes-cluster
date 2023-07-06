23bc4910ef6dc5c7ad2a7933c1f14947dd660998
Venkata Mutyala <venkata@venkatamutyala.com>
2023-07-06 15:10:28 -0700

    feat: adding ec2 ssh support (#40)
    * feat: adding support for adding ssh key pairs to the worker nodes
    * fix: added ami_release_version to the nodepool definition
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- tests/main.tf
+++ tests/main.tf
@@ -1,20 +1,21 @@
 module "captain" {
-  iam_role_to_assume           = "arn:aws:iam::761182885829:role/glueops-captain"
-  source                       = "../"
-  eks_version                  = "1.27"
-  csi_driver_version           = "v1.20.0-eksbuild.1"
-  vpc_cidr_block               = "10.65.0.0/26"
-  region                       = "us-west-2"
-  availability_zones           = ["us-west-2a", "us-west-2b"]
+  iam_role_to_assume = "arn:aws:iam::761182885829:role/glueops-captain"
+  source             = "../"
+  eks_version        = "1.27"
+  csi_driver_version = "v1.20.0-eksbuild.1"
+  vpc_cidr_block     = "10.65.0.0/26"
+  region             = "us-west-2"
+  availability_zones = ["us-west-2a", "us-west-2b"]
   node_pools = [
     # {
-    #   "ami_image_id" : "amazon-eks-node-1.27-v20230607",
+    #   "ami_release_version" : "1.27.1-20230703",
     #   "instance_type" : "t3a.small",
     #   "name" : "clusterwide-node-pool-1",
     #   "node_count" : 2,
     #   "spot" : false,
     #   "disk_size_gb" : 20,
-    #   "max_pods" : 1000
+    #   "max_pods" : 1000,
+    #   "ssh_key_pair_names" : []
     # }
   ]
 }
