d45d7de53d6e98ee783a8dddfa736e146db62416
Venkata Mutyala <venkata@venkatamutyala.com>
2023-08-02 16:27:00 -0700

    Update destroy-aws.sh (#44)
    * Update destroy-aws.sh
    
    * Update aws-cloud-regression-suite.yml

--- tests/destroy-aws.sh
+++ tests/destroy-aws.sh
@@ -2,5 +2,5 @@
 
 # reference: https://github.com/GlueOps/scripts-teardown-aws-amazon-web-services
 echo "Preform an AWS Cleanup with AWS Nuke"
-wget https://github.com/rebuy-de/aws-nuke/releases/download/v2.22.1/aws-nuke-v2.22.1-linux-amd64.tar.gz && tar -xvf aws-nuke-v2.22.1-linux-amd64.tar.gz && rm aws-nuke-v2.22.1-linux-amd64.tar.gz && mv aws-nuke-v2.22.1-linux-amd64 aws-nuke
+wget https://github.com/rebuy-de/aws-nuke/releases/download/v2.23.0/aws-nuke-v2.23.0-linux-amd64.tar.gz && tar -xvf aws-nuke-v2.23.0-linux-amd64.tar.gz && rm aws-nuke-v2.23.0-linux-amd64.tar.gz && mv aws-nuke-v2.23.0-linux-amd64 aws-nuke
 ./aws-nuke -c aws-nuke.yaml --no-dry-run --force
