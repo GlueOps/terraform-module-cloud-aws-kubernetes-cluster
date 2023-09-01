9422e6b006c7fb193f6f1a07ddff7de25bda1630
Venkata Mutyala <venkata@venkatamutyala.com>
2023-09-01 14:15:08 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 
 variable "csi_driver_version" {
   type        = string
-  default     = "v1.21.0-eksbuild.1"
+  default     = "v1.22.0-eksbuild.1"
   description = "You should grab the appropriate version number from: https://github.com/kubernetes-sigs/aws-ebs-csi-driver/blob/master/CHANGELOG.md"
 }
 
