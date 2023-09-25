ad0403fc34265df7c5f6fdcd5ea41a0578576fe7
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-09-25 16:21:53 -0700

    feat: update ami for EKS

--- tests/main.tf
+++ tests/main.tf
@@ -8,7 +8,7 @@ module "captain" {
   availability_zones = ["us-west-2a", "us-west-2b"]
   node_pools = [
 #    {
-#      "ami_image_id" : "ami-0bce9ab1f1be3282a",
+#      "ami_image_id" : "ami-0d62cf3c1488af0a3",
 #      "instance_type" : "t3a.small",
 #      "name" : "glueops-platform-node-pool-1",
 #      "node_count" : 2,
@@ -28,7 +28,7 @@ module "captain" {
 #      ]
 #    },
 #    {
-#      "ami_image_id" : "ami-0bce9ab1f1be3282a",
+#      "ami_image_id" : "ami-0d62cf3c1488af0a3",
 #      "instance_type" : "t3a.small",
 #      "name" : "clusterwide-node-pool-1",
 #      "node_count" : 2,
