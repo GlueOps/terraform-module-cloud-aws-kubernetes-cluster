dd745ef35ee3a2537897564312cd3f2adc3c3607
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-10-13 17:30:21 -0600

    chore(ami_image_id): ami-0d62cf3c1488af0a3 -> ami-084cf519d356bb718

--- tests/main.tf
+++ tests/main.tf
@@ -8,7 +8,7 @@ module "captain" {
   availability_zones = ["us-west-2a", "us-west-2b"]
   node_pools = [
 #    {
-#      "ami_image_id" : "ami-0d62cf3c1488af0a3",
+#      "ami_image_id" : "ami-084cf519d356bb718",
 #      "instance_type" : "t3a.small",
 #      "name" : "glueops-platform-node-pool-1",
 #      "node_count" : 2,
@@ -28,7 +28,7 @@ module "captain" {
 #      ]
 #    },
 #    {
-#      "ami_image_id" : "ami-0d62cf3c1488af0a3",
+#      "ami_image_id" : "ami-084cf519d356bb718",
 #      "instance_type" : "t3a.small",
 #      "name" : "clusterwide-node-pool-1",
 #      "node_count" : 2,
--- variables.tf
+++ variables.tf
@@ -84,7 +84,7 @@ variable "node_pools" {
     name               = "default-pool"
     node_count         = 1
     instance_type      = "t3a.large"
-    ami_image_id       = "ami-0d62cf3c1488af0a3"
+    ami_image_id       = "ami-084cf519d356bb718"
     spot               = false
     disk_size_gb       = 20
     max_pods           = 110
