704a0fff47953a72fbafc31c1ddfa19239d5b699
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-10-14 13:51:51 +0000

    feat(tests): more groups

--- tests/run.sh
+++ tests/run.sh
@@ -2,7 +2,9 @@
 
 set -e
 
+echo "::group::Destroy anything left running"
 ./destroy-aws.sh
+echo "::endgroup::"
 
 echo "::group::Deploying Kubernetes"
 echo "Terraform Init"
@@ -12,6 +14,7 @@ terraform plan
 echo "Terraform Apply"
 terraform apply -auto-approve
 echo "::endgroup::"
+
 echo "::group::Configuring Kubernetes"
 echo "Authenticate with Kubernetes"
 aws eks update-kubeconfig --region us-west-2 --name captain-cluster --role-arn arn:aws:iam::761182885829:role/glueops-captain
@@ -22,6 +25,7 @@ helm repo add projectcalico https://docs.tigera.io/calico/charts
 helm repo update
 helm install calico projectcalico/tigera-operator --version v3.26.1 --namespace tigera-operator -f calico.yaml --create-namespace
 echo "::endgroup::"
+
 echo "::group::Deploying new Node Pool"
 echo "Deploy node pool"
 sed -i 's/#//g' main.tf
@@ -30,11 +34,13 @@ echo "Get nodes and pods from kubernetes"
 kubectl get nodes
 kubectl get pods -A -o=wide
 echo "::endgroup::"
+
 echo "::group::Start Test Suite"
 echo ""
 ./k8s-test.sh
 echo "Test Suite Complete"
 echo "::endgroup::"
+
 echo "::group::Tear down Environment"
 echo "Terraform Destroy"
 terraform destroy -auto-approve
