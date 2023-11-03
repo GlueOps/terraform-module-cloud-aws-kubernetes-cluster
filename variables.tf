3e9a7e4c1e46352d8ff4c8b71618a487d0790d23
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-11-03 13:10:36 -0600

    chore(csi_driver_version): v1.24.0-eksbuild.1 -> v1.24.1-eksbuild.1 (#65)

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 
 variable "csi_driver_version" {
   type        = string
-  default     = "v1.24.0-eksbuild.1"
+  default     = "v1.24.1-eksbuild.1"
   description = "You should grab the appropriate version number from: https://github.com/kubernetes-sigs/aws-ebs-csi-driver/blob/master/CHANGELOG.md"
 }
 
