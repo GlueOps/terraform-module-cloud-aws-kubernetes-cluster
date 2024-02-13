15f443d9f5a4f888dd82b5254e5023325054b30d
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2024-02-12 19:26:30 -0800

    chore(csi_driver_version): v1.26.1-eksbuild.1 -> v1.27.0-eksbuild.1

--- tests/main.tf
+++ tests/main.tf
@@ -2,7 +2,7 @@ module "captain" {
   iam_role_to_assume = "arn:aws:iam::761182885829:role/glueops-captain-role" 
   source             = "../"
   eks_version        = "1.27"
-  csi_driver_version = "v1.26.1-eksbuild.1"
+  csi_driver_version = "v1.27.0-eksbuild.1"
   coredns_version    = "v1.10.1-eksbuild.6"
   vpc_cidr_block     = "10.65.0.0/26"
   region             = "us-west-2"
