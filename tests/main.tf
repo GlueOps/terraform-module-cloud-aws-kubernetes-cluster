38af4af0d64198e0c4ac5de3484e6c4ccac9c56b
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2024-03-19 09:57:07 -0700

    chore(csi_driver_version): v1.27.0-eksbuild.1 -> v1.28.0-eksbuild.1 (#89)
    * chore(csi_driver_version): v1.27.0-eksbuild.1 -> v1.28.0-eksbuild.1
    
    * terraform-docs: automated action
    
    * chore(codespace-version): v0.36.0 -> v0.37.0
    
    * chore(ami_image_id): ami-00f90c71528d11250 ->ami-0669c37b51df72204
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- tests/main.tf
+++ tests/main.tf
@@ -2,14 +2,14 @@ module "captain" {
   iam_role_to_assume = "arn:aws:iam::761182885829:role/glueops-captain-role" 
   source             = "../"
   eks_version        = "1.27"
-  csi_driver_version = "v1.27.0-eksbuild.1"
+  csi_driver_version = "v1.28.0-eksbuild.1"
   coredns_version    = "v1.10.1-eksbuild.7"
   vpc_cidr_block     = "10.65.0.0/26"
   region             = "us-west-2"
   availability_zones = ["us-west-2a", "us-west-2b"]
   node_pools = [
 #    {
-#      "ami_image_id" : "ami-00f90c71528d11250",
+#      "ami_image_id" : "ami-0669c37b51df72204",
 #      "instance_type" : "t3a.xlarge",
 #      "name" : "glueops-platform-node-pool-1",
 #      "node_count" : 4,
@@ -29,7 +29,7 @@ module "captain" {
 #      ]
 #    },
 #    {
-#      "ami_image_id" : "ami-00f90c71528d11250",
+#      "ami_image_id" : "ami-0669c37b51df72204",
 #      "instance_type" : "t3a.large",
 #      "name" : "clusterwide-node-pool-1",
 #      "node_count" : 4,
