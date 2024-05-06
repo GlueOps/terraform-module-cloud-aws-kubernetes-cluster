4184b3696423ec774cca1e1042ea227474580e2e
Venkat <venkata@venkatamutyala.com>
2024-05-06 15:31:29 -0700

    feat: upgrading from eks 1.27 -> 1.28

--- tests/main.tf
+++ tests/main.tf
@@ -1,15 +1,15 @@
 module "captain" {
   iam_role_to_assume = "arn:aws:iam::761182885829:role/glueops-captain-role" 
   source             = "../"
-  eks_version        = "1.27"
-  csi_driver_version = "v1.29.1-eksbuild.1"
-  coredns_version    = "v1.10.1-eksbuild.7"
+  eks_version        = "1.28"
+  csi_driver_version = "v1.30.0-eksbuild.1"
+  coredns_version    = "v1.10.1-eksbuild.10"
   vpc_cidr_block     = "10.65.0.0/26"
   region             = "us-west-2"
   availability_zones = ["us-west-2a", "us-west-2b"]
   node_pools = [
 #    {
-#      "ami_image_id" : "ami-05eb3a655e73909a1",
+#      "ami_image_id" : "ami-077ca19cf151a75e0",
 #      "instance_type" : "t3a.xlarge",
 #      "name" : "glueops-platform-node-pool-1",
 #      "node_count" : 4,
@@ -29,7 +29,7 @@ module "captain" {
 #      ]
 #    },
 #    {
-#      "ami_image_id" : "ami-05eb3a655e73909a1",
+#      "ami_image_id" : "ami-077ca19cf151a75e0",
 #      "instance_type" : "t3a.large",
 #      "name" : "clusterwide-node-pool-1",
 #      "node_count" : 4,
