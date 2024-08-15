094bfaab118282c747b06e63f25ee746f21f6cd5
Venkat <venkata@venkatamutyala.com>
2024-08-14 22:19:37 -0700

    fix: create_before_destroy = false otherwise it adds a pet_name. We may want to set this to true later

--- node_pool.tf
+++ node_pool.tf
@@ -3,18 +3,19 @@ module "node_pool" {
   for_each = { for np in var.node_pools : np.name => np }
   source   = "cloudposse/eks-node-group/aws"
   # Cloud Posse recommends pinning every module to a specific version
-  version           = "3.1.0"
-  ec2_ssh_key_name  = each.value.ssh_key_pair_names
-  instance_types    = [each.value.instance_type]
-  subnet_ids        = module.subnets.public_subnet_ids
-  desired_size      = each.value.node_count
-  min_size          = each.value.node_count
-  max_size          = each.value.node_count + 1
-  cluster_name      = module.kubernetes.eks_cluster_id
-  capacity_type     = each.value.spot ? "SPOT" : "ON_DEMAND"
-  ami_image_id      = [each.value.ami_image_id]
-  kubernetes_labels = each.value.kubernetes_labels
-  kubernetes_taints = each.value.kubernetes_taints
+  version               = "3.1.0"
+  ec2_ssh_key_name      = each.value.ssh_key_pair_names
+  instance_types        = [each.value.instance_type]
+  subnet_ids            = module.subnets.public_subnet_ids
+  desired_size          = each.value.node_count
+  min_size              = each.value.node_count
+  max_size              = each.value.node_count + 1
+  cluster_name          = module.kubernetes.eks_cluster_id
+  capacity_type         = each.value.spot ? "SPOT" : "ON_DEMAND"
+  ami_image_id          = [each.value.ami_image_id]
+  kubernetes_labels     = each.value.kubernetes_labels
+  kubernetes_taints     = each.value.kubernetes_taints
+  create_before_destroy = false
 
   cluster_autoscaler_enabled = false
   name                       = each.value.name
