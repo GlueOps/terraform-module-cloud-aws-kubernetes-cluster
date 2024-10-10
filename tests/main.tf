6f0f864f4ad953e15610a7f7aa3cd4ec8e9bc9b3
Venkat <venkata@venkatamutyala.com>
2024-10-10 11:54:41 -0700

    feat: updates (#163)
    * feat: update ekis AMI
    
    * feat: update versions
    
    * chore: add comment to ensure consistentcy of updates
    
    Co-authored-by: codiumai-pr-agent-free[bot] <138128286+codiumai-pr-agent-free[bot]@users.noreply.github.com>
    
    * fix: remove comment as it breaks existing scripts. We can add it back later as needed
    
    ---------
    
    Co-authored-by: codiumai-pr-agent-free[bot] <138128286+codiumai-pr-agent-free[bot]@users.noreply.github.com>

--- tests/main.tf
+++ tests/main.tf
@@ -2,16 +2,16 @@ module "captain" {
   iam_role_to_assume = "arn:aws:iam::761182885829:role/glueops-captain-role"
   source             = "../"
   eks_version        = "1.28"
-  csi_driver_version = "v1.34.0-eksbuild.1"
+  csi_driver_version = "v1.35.0-eksbuild.1"
   coredns_version    = "v1.10.1-eksbuild.13"
-  kube_proxy_version = "v1.28.12-eksbuild.2"
+  kube_proxy_version = "v1.28.12-eksbuild.5"
   vpc_cidr_block     = "10.65.0.0/26"
   region             = "us-west-2"
   availability_zones = ["us-west-2a", "us-west-2b"]
   node_pools = [
     #    {
     #      "kubernetes_version" : "1.28",
-    #      "ami_release_version" : "1.28.11-20240828",
+    #      "ami_release_version" : "1.28.13-20240928",
     #      "ami_type" : "AL2_x86_64",
     #      "instance_type" : "t3a.large",
     #      "name" : "glueops-platform-node-pool-1",
@@ -33,7 +33,7 @@ module "captain" {
     #    },
     #    {
     #      "kubernetes_version" : "1.28",
-    #      "ami_release_version" : "1.28.11-20240828",
+    #      "ami_release_version" : "1.28.13-20240928",
     #      "ami_type" : "AL2_x86_64",
     #      "instance_type" : "t3a.small",
     #      "name" : "glueops-platform-node-pool-argocd-app-controller-1",
@@ -55,7 +55,7 @@ module "captain" {
     #    },
     #    {
     #      "kubernetes_version" : "1.28",
-    #      "ami_release_version" : "1.28.11-20240828",
+    #      "ami_release_version" : "1.28.13-20240928",
     #      "ami_type" : "AL2_x86_64",
     #      "instance_type" : "t3a.medium",
     #      "name" : "clusterwide-node-pool-1",
