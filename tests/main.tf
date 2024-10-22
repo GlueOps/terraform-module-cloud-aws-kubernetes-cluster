1ac6766528eb270c9ffa45e2744aef7bec90578f
Venkat <venkata@venkatamutyala.com>
2024-10-22 10:56:50 -0700

    feat: update eks csi driver -> v1.36.0-eksbuild.1

--- tests/main.tf
+++ tests/main.tf
@@ -2,7 +2,7 @@ module "captain" {
   iam_role_to_assume = "arn:aws:iam::761182885829:role/glueops-captain-role"
   source             = "../"
   eks_version        = "1.28"
-  csi_driver_version = "v1.35.0-eksbuild.1"
+  csi_driver_version = "v1.36.0-eksbuild.1"
   coredns_version    = "v1.10.1-eksbuild.13"
   kube_proxy_version = "v1.28.12-eksbuild.5"
   vpc_cidr_block     = "10.65.0.0/26"
