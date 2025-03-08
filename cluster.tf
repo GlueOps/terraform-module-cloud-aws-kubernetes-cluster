4d97c1d8c41e14e2d9d23c6b3d94ddb7738943de
Bazmahou <hamzabouissi@sousse.r-iset.tn>
2025-03-08 11:55:50 +0000

    change subnet ids

--- cluster.tf
+++ cluster.tf
@@ -4,7 +4,7 @@ module "kubernetes" {
 
   region     = var.region
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.subnets.private_subnet_ids
+  subnet_ids = concat(module.subnets.private_subnet_ids, module.subnets.public_subnet_ids)
 
   oidc_provider_enabled      = true
   name                       = "captain"
