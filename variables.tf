0796bc6ac12b887362447e6dc15373878d0de09b
Venkat <venkata@venkatamutyala.com>
2026-04-30 15:19:06 -0700

    fix: variables.tf

--- variables.tf
+++ variables.tf
@@ -97,8 +97,8 @@ variable "node_pools" {
     spot                             = bool
     disk_size_gb                     = number
     max_pods                         = number
-    enable_ssm                       = false
-    enable_cve_2026_31431_mitigation = false
+    enable_ssm                       = bool
+    enable_cve_2026_31431_mitigation = bool
     ssh_key_pair_names               = list(string)
     kubernetes_labels                = map(string)
     kubernetes_taints = list(object({
